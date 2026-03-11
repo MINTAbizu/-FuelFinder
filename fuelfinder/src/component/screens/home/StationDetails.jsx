6350835f6dfd9afb6c382061b561721e75be7b6c
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2026-03-10 17:28:09 -0700

    Update StationDetails.jsx

--- fuelfinder/src/component/screens/home/StationDetails.jsx
+++ fuelfinder/src/component/screens/home/StationDetails.jsx
@@ -67,6 +67,13 @@ function normalizeTicketPayload(ticket) {
   };
 }
 
+function formatDateTime(value) {
+  if (!value) return "-";
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) return "-";
+  return date.toLocaleString();
+}
+
 export default function StationDetails({ route }) {
   const { t: tr } = useLanguage();
   const { user } = useAuth();
